731935717fc9803a151e979005728dc45ec21981
Kyle Herrity <kherrity@gmail.com>
2018-12-17 21:51:16 -0600

    Frog feeder initial commit. Simple Arduino servo controller to feed a frog.

---
+++ frog_feeder/frog_feeder.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo servo;
+
+const int neutral_pos = 10; // setting to zero causes servo vibration
+
+void setup() {
+  servo.attach(9); // digital pin 9
+  servo.write(neutral_pos);
+  delay(5000);
+}
+
+void servo_tilt(int degrees) {
+  servo.write(neutral_pos + degrees);
+}
+
+// Feed Mr. Know-It-All every 24 hours
+unsigned long interval = 24UL * 60UL * 60UL * 1000UL;
+
+void loop() {
+  servo_tilt(105);
+  delay(500);
+  servo_tilt(0);
+  unsigned long startMillis = millis();
+  while (millis() - startMillis < interval);
+}
